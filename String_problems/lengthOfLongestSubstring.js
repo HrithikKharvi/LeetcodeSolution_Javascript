0850147ca88e299d5488c2796cab0080b553bf08
Hrithik R <hrithikkharvi9@gmail.com>
2022-12-12 20:29:43 +0530

    First Javascript Leetcode solution commit

---
+++ String_problems/lengthOfLongestSubstring.js
@@ -0,0 +1,29 @@
+var lengthOfLongestSubstring = function(s) {
+	   
+   let pointer1 = 0;
+  let pointer2 = 0;
+  let maxCount = 0;
+
+  let map = {};
+
+  while(pointer2 < s.length){
+    let currentString = s[pointer2];
+    let seenIndex = map[currentString];
+    if(seenIndex == undefined){
+      map[currentString] = pointer2;
+      pointer2++;
+    }else{
+      if(seenIndex >= pointer1){
+        pointer1 = seenIndex+1;
+      }
+        map[currentString] = pointer2;
+
+      pointer2++;
+    }
+
+        let currentCount = (pointer2 - 1) - pointer1 +1;
+        maxCount = Math.max(currentCount, maxCount);
+  }
+
+  return maxCount;
+};
